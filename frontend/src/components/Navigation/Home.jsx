0e9cac326c94844cf819ef96f999681fa06ed042
Timofey <timofey.syr17@gmail.com>
2024-04-26 22:20:32 +0300

    TOC 12 - homepage

--- frontend/src/components/Navigation/Home.jsx
+++ frontend/src/components/Navigation/Home.jsx
@@ -1,5 +1,10 @@
 import { NavLink } from 'react-router-dom'
 import React, { useState } from 'react'
+// eslint-disable-next-line
+import { Link, animateScroll as scroll } from 'react-scroll'
+import Slider from 'react-slick'
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
 import TrackFormPopup from '../TrackFormPopup/TrackFormPopup'
 import './Home.css'
 import ImageSlider from '../Sliders/ImageSlider'
@@ -31,12 +36,49 @@ const Home = () => {
   const toggleShowMore = () => {
     setShowMore(!showMore)
   }
-
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 200,
+    slidesToShow: 5,
+    slidesToScroll: 2,
+    arrows: false,
+  }
   return (
     <>
-      <div className="page-nav">
-        <a href="#track-record">Запись трека</a>
-        <a href="#udarnie">Ударные</a>
+      <div>
+        <Slider {...settings} className="page-nav">
+          <Link
+            to="track-record"
+            smooth={true}
+            duration={500}
+            className="nav-link"
+          >
+            Запись трека
+          </Link>
+          <Link to="udarnie" smooth={true} duration={500} className="nav-link">
+            Ударные
+          </Link>
+          <Link
+            to="sintesator"
+            smooth={true}
+            duration={500}
+            className="nav-link"
+          >
+            Синтезаторы
+          </Link>
+          <Link
+            to="bass-guitar"
+            smooth={true}
+            duration={500}
+            className="nav-link"
+          >
+            Бас-гитары
+          </Link>
+          <Link to="kardan" smooth={true} duration={500} className="nav-link">
+            Кардан
+          </Link>
+        </Slider>
       </div>
       <div className="home-container">
         <div className="track-record-title" id="track-record">
@@ -46,12 +88,14 @@ const Home = () => {
           <ImageSlider images={images} className="slide-image" />
         </div>
         <div className="track-record-description">
-          В нашей студии звукозаписи каждая нота находит свой путь к
-          совершенству. Мы обеспечиваем профессиональное звучание вашей музыке,
-          используя только лучшее оборудование и передовые технологии. Наши
-          звукоинженеры воплощают в жизнь любые ваши идеи, от первых аккордов до
-          финального мастеринга. Запишите свой трек у нас и позвольте вашему
-          творчеству зазвучать на весь мир!
+          <p>
+            В нашей студии звукозаписи каждая нота находит свой путь к
+            совершенству. Мы обеспечиваем профессиональное звучание вашей
+            музыке, используя только лучшее оборудование и передовые технологии.
+            Наши звукоинженеры воплощают в жизнь любые ваши идеи, от первых
+            аккордов до финального мастеринга. Запишите свой трек у нас и
+            позвольте вашему творчеству зазвучать на весь мир!
+          </p>
         </div>
         <div className="app-track-record">
           <button onClick={openPopup}>Записаться!</button>
@@ -74,14 +118,14 @@ const Home = () => {
           <ImageSlider images={imagesUdarnie} className="slide-image" />
         </div>
         <div className="track-record-description">
-          <span>
+          <p>
             В ритме каждого сердцебиения, в каждом звуке, затаиваются мечты и
             эмоции. Ударные инструменты - душа ритма, сердце музыки. И когда
             речь заходит о профессиональной ударной установке, Pearl выходит
             вперед, как символ качества и инноваций. Наша инструментальная база
             представляет собой не просто коллекцию барабанов и тарелок, она -
             воплощение стиля и мощи.
-          </span>
+          </p>
 
           {!showMore && (
             <button onClick={toggleShowMore} className="show-more-button">
@@ -125,12 +169,216 @@ const Home = () => {
             </div>
           )}
         </div>
+
         <div className="app-track-record">
           <button onClick={openPopup}>Записаться!</button>
         </div>
         <div className="track-record-price">
-          <NavLink to="/contacts">Стоимость</NavLink> от 400р / 2 часа
+          <NavLink to="/contacts">Стоимость</NavLink> допа от 100р / 2 часа
         </div>
+      </div>
+      <hr />
+      <div className="home-container">
+        <div className="track-record-title" id="sintesator">
+          Наша инструментальная база: Синтезаторы
+        </div>
+        <div className="image-slider-container">
+          <ImageSlider images={images} className="slide-image" />
+        </div>
+        <div className="track-record-description">
+          <p>
+            Добро пожаловать в нашу звукозаписывающую студию, где мы
+            предоставляем вам доступ к самой передовой инструментальной базе
+            синтезаторов. Наша студия оснащена широким ассортиментом
+            синтезаторов, от классических аналоговых моделей до современных
+            цифровых инструментов, готовых воплотить ваши музыкальные идеи в
+            жизнь. Наша цель - обеспечить вас всем необходимым для создания
+            выдающихся звукозаписей. Мы предлагаем не только самые современные
+            синтезаторы, но и профессиональное оборудование и экспертную
+            поддержку наших звукорежиссеров и продюсеров.
+          </p>
+          {!showMore && (
+            <button onClick={toggleShowMore} className="show-more-button">
+              <u>Узнать больше</u>
+            </button>
+          )}
+
+          {showMore && (
+            <div className="additional-content">
+              <p>
+                В нашей студии вы найдете все, что нужно для полного воплощения
+                вашего музыкального видения. Независимо от того, создаете ли вы
+                электронную музыку, саундтреки для фильмов, аудиорекламу или
+                любую другую форму аудиоискусства, наши синтезаторы станут
+                надежным и вдохновляющим инструментом в вашем творчестве.
+              </p>
+
+              <p>
+                Мы также предлагаем гибкие условия аренды студии, чтобы
+                соответствовать вашему графику и бюджету. Наша команда всегда
+                готова помочь вам воплотить ваши музыкальные идеи в реальность,
+                обеспечивая вас всем необходимым для успешной звукозаписи.
+              </p>
+              <p>
+                Выберите нашу звукозаписывающую студию с синтезаторами в
+                качестве вашего творческого партнера, и давайте вместе создадим
+                что-то удивительное. Ваша музыка заслуживает самого лучшего, и
+                мы здесь, чтобы обеспечить вас этим.
+              </p>
+              {showMore && (
+                <button onClick={toggleShowMore} className="show-more-button">
+                  <u>Скрыть текст</u>
+                </button>
+              )}
+            </div>
+          )}
+        </div>
+        <div className="app-track-record">
+          <button onClick={openPopup}>Записаться!</button>
+        </div>
+        <div className="track-record-price">
+          <NavLink to="/contacts">Стоимость</NavLink>допа от 100р / 2 часа
+        </div>
+
+        <hr className="horizontal-divider" />
+        {isPopupOpen && (
+          <TrackFormPopup onClose={() => setIsPopupOpen(false)} />
+        )}
+      </div>
+
+      <div className="home-container">
+        <div className="track-record-title" id="bass-guitar">
+          Наша инструментальная база: Бас-гитары
+        </div>
+        <div className="image-slider-container">
+          <ImageSlider images={images} className="slide-image" />
+        </div>
+        <div className="track-record-description">
+          <p>
+            Добро пожаловать в нашу звукозаписывающую студию, где мы предлагаем
+            вам доступ к нашей впечатляющей инструментальной базе бас-гитар. Мы
+            гордимся тем, что предоставляем музыкантам возможность работать с
+            самыми качественными и разнообразными инструментами, которые
+            подчеркнут уникальность вашего звучания.
+          </p>
+          {!showMore && (
+            <button onClick={toggleShowMore} className="show-more-button">
+              <u>Узнать больше</u>
+            </button>
+          )}
+
+          {showMore && (
+            <div className="additional-content">
+              <p>
+                В нашей студии вы найдете широкий выбор бас-гитар, от
+                классических моделей до современных инновационных дизайнов.
+                Независимо от того, ищете ли вы глубокий и насыщенный бас для
+                рока, мягкие и плавные звуки для джаза или энергичные ритмы для
+                поп-музыки, у нас есть инструменты, которые подойдут именно под
+                ваш проект.
+              </p>
+
+              <p>
+                Мы также предлагаем услуги наших опытных звукорежиссеров и
+                продюсеров, которые помогут вам достичь идеального звучания
+                вашего баса. Наша команда готова поддержать вас на каждом этапе
+                процесса записи, обеспечивая профессиональное качество и
+                внимательное отношение к вашему творчеству.
+              </p>
+              <p>
+                Мы ценим ваше стремление к качественной музыке, поэтому
+                предлагаем гибкие условия аренды студии, чтобы соответствовать
+                вашим потребностям и бюджету. Выберите нашу звукозаписывающую
+                студию с бас-гитарами в качестве вашего творческого партнера, и
+                вместе мы создадим незабываемую музыку, которая будет звучать
+                ярко и запоминающе.
+              </p>
+              {showMore && (
+                <button onClick={toggleShowMore} className="show-more-button">
+                  <u>Скрыть текст</u>
+                </button>
+              )}
+            </div>
+          )}
+        </div>
+        <div className="app-track-record">
+          <button onClick={openPopup}>Записаться!</button>
+        </div>
+        <div className="track-record-price">
+          <NavLink to="/contacts">Стоимость</NavLink>допа от 100р / 2 часа
+        </div>
+
+        <hr className="horizontal-divider" />
+        {isPopupOpen && (
+          <TrackFormPopup onClose={() => setIsPopupOpen(false)} />
+        )}
+      </div>
+
+      <div className="home-container">
+        <div className="track-record-title" id="kardan">
+          Наша инструментальная база: Кардан
+        </div>
+        <div className="image-slider-container">
+          <ImageSlider images={images} className="slide-image" />
+        </div>
+        <div className="track-record-description">
+          <p>
+            Добро пожаловать в нашу звукозаписывающую студию, где мы гордимся
+            нашей впечатляющей инструментальной базой карданов. Кардан – это
+            инструмент, который придает вашей музыке особую глубину и характер,
+            и мы рады предоставить вам доступ к лучшим образцам этого
+            уникального инструмента.
+          </p>
+          {!showMore && (
+            <button onClick={toggleShowMore} className="show-more-button">
+              <u>Узнать больше</u>
+            </button>
+          )}
+
+          {showMore && (
+            <div className="additional-content">
+              <p>
+                Наша студия оснащена широким ассортиментом карданов,
+                представленных в различных формах и конфигурациях. От
+                классических деревянных до современных композитных моделей, у
+                нас есть инструменты, которые подойдут для широкого спектра
+                музыкальных жанров и стилей.
+              </p>
+
+              <p>
+                Помимо высококачественных инструментов, мы также предоставляем
+                услуги наших опытных звукорежиссеров и инженеров звука, чтобы
+                обеспечить вас всем необходимым для успешной звукозаписи. Наша
+                студия создана для воплощения ваших творческих идей в
+                реальность, предоставляя вам самые лучшие инструменты и
+                профессиональную поддержку.
+              </p>
+              <p>
+                Выберите нашу звукозаписывающую студию с карданами в качестве
+                вашего творческого партнера, и давайте вместе создадим музыку,
+                которая будет вдохновлять и оставаться в памяти вашей аудитории.
+                Ваше музыкальное видение заслуживает лучших инструментов, и мы
+                здесь, чтобы обеспечить вас ими.
+              </p>
+              {showMore && (
+                <button onClick={toggleShowMore} className="show-more-button">
+                  <u>Скрыть текст</u>
+                </button>
+              )}
+            </div>
+          )}
+        </div>
+        <div className="app-track-record">
+          <button onClick={openPopup}>Записаться!</button>
+        </div>
+        <div className="track-record-price">
+          <NavLink to="/contacts">Стоимость</NavLink>допа от 50р / 2 часа
+        </div>
+
+        <hr className="horizontal-divider" />
+        {isPopupOpen && (
+          <TrackFormPopup onClose={() => setIsPopupOpen(false)} />
+        )}
       </div>
     </>
   )
