ad5978c64c073d41c4ee1c84c4131551c7f1f066
Timofey <timofey.syr17@gmail.com>
2024-05-04 15:14:59 +0300

    ui fix

--- frontend/src/components/Navigation/ContactsComponents/About.jsx
+++ frontend/src/components/Navigation/ContactsComponents/About.jsx
@@ -20,7 +20,7 @@ const features = [
   {
     name: 'Инструментальная база',
     description:
-      'У нас есть большое количество инструментов, которые мы сдаем в аренду.',
+      'У нас есть большое количество инструментов, которые мы сдаем в',
     icon: SpeakerWaveIcon,
   },
 ]
@@ -32,7 +32,7 @@ export default function About() {
         <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
           <div className="lg:pr-8 lg:pt-4">
             <div className="lg:max-w-lg">
-              <h2 className="text-base font-semibold leading-7 text-indigo-600">
+              <h2 className="text-base font-semibold leading-7 text-about-text">
                 Создавай прекрасное
               </h2>
               <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
@@ -47,7 +47,7 @@ export default function About() {
                   <div key={feature.name} className="relative pl-9">
                     <dt className="inline font-semibold text-gray-900">
                       <feature.icon
-                        className="absolute left-1 top-1 h-5 w-5 text-indigo-600"
+                        className="absolute left-1 top-1 h-5 w-5 text-about-text"
                         aria-hidden="true"
                       />
                       {feature.name}
