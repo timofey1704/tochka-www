7dca80d31ab6eda05f6f0f86c639f2b08d8d2714
Timofey <timofey.syr17@gmail.com>
2024-11-01 10:58:42 +0300

    TOC-3 create shop classes and connect to admin panel

--- backend/shop/admin.py
+++ backend/shop/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+admin.site.register(models.Clients)
+admin.site.register(models.Instruments)
+admin.site.register(models.Requests)
+admin.site.register(models.Schedule)
+admin.site.register(models.Texts)
--- backend/shop/models.py
+++ backend/shop/models.py
@@ -1,3 +1,65 @@
 from django.db import models
 
-# Create your models here.
+# models list:
+# clients
+# instruments
+# requests
+# schedule
+# sendMessage
+# texts
+
+class Clients(models.Model):
+    title = models.CharField(max_length=255)
+    img_url = models.CharField(max_length=255)
+    url = models.CharField(max_length=255)
+    class Meta:
+        verbose_name = "Клиент"
+        verbose_name_plural = "Клиенты"
+    
+    def __str__(self):
+        return self.title
+class Instruments(models.Model):
+    title = models.CharField(max_length=255)
+    img_url = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    link = models.CharField(max_length=255)
+    price = models.CharField(max_length=255)
+    class Meta:
+        verbose_name = "Инструмент"
+        verbose_name_plural = "Инструменты"
+    
+    def __str__(self):
+        return self.title
+    
+class Requests(models.Model):
+    telegram_id = models.CharField(max_length=255)
+    date = models.DateTimeField()
+    phone = models.CharField(max_length=255)
+    end_time = models.CharField(max_length=255)
+    class Meta:
+        verbose_name = "Запись"
+        verbose_name_plural = "Записи"
+    
+    def __str__(self):
+        return self.telegram_id
+    
+class Schedule(models.Model):
+    date = models.DateTimeField()
+    time = models.CharField(max_length=255)
+    end_time = models.CharField(max_length=255)
+    class Meta:
+        verbose_name = "Расписание"
+        verbose_name_plural = "Расписания"
+    
+    def __str__(self):
+        return self.date
+    
+class Texts(models.Model):
+    text = models.CharField(max_length=1000)
+    path = models.CharField(max_length=20)
+    class Meta:
+        verbose_name = "Текст"
+        verbose_name_plural = "Тексты"
+    
+    def __str__(self):
+        return self.path
